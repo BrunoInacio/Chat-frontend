6087362bb54f79a1ac68e24c9fb38c6db7d8370e
Bruno Inácio <brunob.inacio@gmail.com>
2020-04-04 22:34:29 -0300

    Update project structure Add Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./style.scss"
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p>Projeto de Formatura - PCS-USP</p>
+    </div>
+  );
+}
+
+export default Footer;
