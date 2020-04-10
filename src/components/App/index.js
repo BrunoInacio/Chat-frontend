035b057cb0a37298d5a50e5c9c994eb8e2066bd1
Bruno Inácio <brunob.inacio@gmail.com>
2020-04-10 00:45:31 -0300

    Update project structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,1 @@
-import React from 'react';
-import Footer from '../Footer'
-
-import './style.css';
-
-function App() {
-  return (
-    <div className="App">
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
+export { default } from './App';
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,1 @@
-import React from 'react';
-
-import "./style.scss"
-
-function Footer() {
-  return (
-    <div className="Footer">
-      <p>Projeto de Formatura - PCS-USP</p>
-    </div>
-  );
-}
-
-export default Footer;
+export { default } from './Footer';
