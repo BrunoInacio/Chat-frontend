01b9f7117c9306eec8fb5e4e5c7b27dd77dbaca9
Bruno Inácio <brunob.inacio@gmail.com>
2020-04-10 18:14:28 -0300

    Add Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,22 +1,208 @@
-import React, { Component } from 'react';
-import { Drawer, List, ListItem, ListItemText, Divider, ListItemIcon } from '@material-ui/core';
-import MailIcon from '@material-ui/icons/Mail';
+import React from 'react';
 
-export default class Sidebar extends Component {
-  render() {
-    return (
-      <Drawer variant="permanent" anchor="left">
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon><MailIcon /></ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-      </Drawer>
-    );
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import HomeIcon from '@material-ui/icons/Home';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import HelpIcon from '@material-ui/icons/Help';
+import MailIcon from '@material-ui/icons/Mail';
+import InfoIcon from '@material-ui/icons/Info';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+
+  grow: {
+    flexGrow: 1,
+  },
+
+  footer: {
+    alignSelf: "flex-center",
+    bottom: 0,
   }
+}));
+
+export default function Sidebar() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Chatbot
+          </Typography>
+        </Toolbar>
+        
+      </AppBar>
+      
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+
+
+
+        <Divider />
+
+        <List>
+          <ListItem button key="inicio">
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary="Início" />
+          </ListItem>
+          <ListItem button key="chatbot">
+            <ListItemIcon><QuestionAnswerIcon /></ListItemIcon>
+            <ListItemText primary="Chatbot" />
+          </ListItem>
+        </List>
+
+        <Divider />
+
+        <List>
+          <ListItem button key="duvidas">
+            <ListItemIcon><HelpIcon /></ListItemIcon>
+            <ListItemText primary="Dúvidas comuns" />
+          </ListItem>
+          <ListItem button key="contato">
+            <ListItemIcon><MailIcon /></ListItemIcon>
+            <ListItemText primary="Entrar em contato" />
+          </ListItem>
+          <ListItem button key="info">
+            <ListItemIcon><InfoIcon /></ListItemIcon>
+            <ListItemText primary="Informações" />
+          </ListItem>
+        </List>
+      
+        <div className={classes.grow} />
+        
+        <div className={clsx({
+          [classes.footer]: open,
+          [classes.hide]: !open,
+        })}>
+          <Divider />
+
+          <Typography variant="caption">
+            Projeto de formatura
+          </Typography>
+
+          <Typography paragraph variant="overline">
+            PCS - USP
+          </Typography>
+        </div>
+
+      </Drawer>
+
+    </div>
+  );
 }
