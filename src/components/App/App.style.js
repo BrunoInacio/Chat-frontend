a023e4052a458056d90886261a3fc22ecee895fc
Pedro Ligabue <pliga@globo.com>
2020-05-16 16:11:01 -0300

    Change CSS
    Using Boxes allows for less bloated css.

--- src/components/App/App.style.js
+++ src/components/App/App.style.js
@@ -3,14 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
   root: {
-    display: 'flex',
     height: '100vh',
+    width: '100vw'
   },
   content: {
     position: 'relative',
-    maxWidth: theme.breakpoints.values['lg'],
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    padding: theme.spacing(2, 1, 2, 1),
   }
 }));
