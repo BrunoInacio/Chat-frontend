383684024f95924019901576edd519c0d8866bbf
Pedro Ligabue <pliga@globo.com>
2020-05-31 20:20:39 -0300

    Add useTheme hook to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,21 +7,16 @@ import getTheme from './App.theme'
 import Navigation from '../Navigation';
 import Routes from './App.routes'
 
-import { useWidth, useTitle } from '../../utils';
+import { useWidth, useTheme, useTitle } from '../../utils';
 
 export default function App() {
   const [title, setTitle] = useTitle("");
-  const [Theme, setTheme] = React.useState(getTheme('light'));
+  const [Theme, setTheme] = useTheme(getTheme('light'));
   const isMobile = useWidth() < Theme.breakpoints.values['sm'];
   
   const switchTheme = () => {
     setTheme(Theme.palette.type === 'light' ? getTheme('dark') : getTheme('light'));
   };
-
-  React.useEffect(() => { 
-    document.querySelector("meta[name=theme-color]")
-            .setAttribute("content", Theme.palette.background.header);
-  });
 
   return (
     <MuiThemeProvider theme={Theme}>
