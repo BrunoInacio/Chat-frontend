5e2098cad06af8e1a555f7ca3f3d7c0c4b231a41
Pedro Ligabue <pliga@globo.com>
2020-06-06 01:03:24 -0300

    Update App to include the mobile check in useTheme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,15 @@ import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 
-import useTheme from './App.theme'
+import useTheme from './App.useTheme'
 import Navigation from '../Navigation';
 import Routes from './App.routes'
 
-import { useWidth, useTitle } from '../../utils';
+import { useTitle } from '../../utils';
 
 export default function App() {
   const [title, setTitle] = useTitle("");
-  const [theme, switchTheme] = useTheme();
-  const isMobile = useWidth() < theme.breakpoints.values['sm'];
+  const [theme, isMobile, switchTheme] = useTheme();
 
   return (
     <MuiThemeProvider theme={theme}>
