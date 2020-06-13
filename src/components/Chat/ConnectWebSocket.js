5a299a9bac71a102d08f4e9ccc468f2a5c6c7092
Bruno <brunob.inacio@gmail.com>
2020-06-12 21:19:45 -0300

    Add ConnectedAlert

--- src/components/Chat/ConnectWebSocket.js
+++ src/components/Chat/ConnectWebSocket.js
@@ -1,14 +1,25 @@
 import React  from 'react';
 
 export default function WebSocketAPI() {
-  const websocket = React.useRef(null);
+  const websocket = React.useRef(new WebSocket("ws://localhost:6789"));
+
+  const [isConnected, setConnected] = React.useState(null);
+  websocket.current.isConnected = isConnected;
 
   React.useEffect(() => {
-    websocket.current = new WebSocket("ws://localhost:6789");
-    
-    websocket.current.onopen = () => console.log("CONNECTED");
-    websocket.current.onclose = () => console.log("DISCONNECTED");
-    websocket.current.onerror = () => console.log("ERROR");
+    websocket.current.onopen = () => {
+      setConnected(true);
+      console.log("CONNECTED");
+    }
+
+    websocket.current.onclose = (e) => {
+      setConnected(false);
+      console.log("DISCONNECTED");
+    }
+
+    websocket.current.onerror = (e) => {
+      console.log("ERROR");
+    }
 
     return () => websocket.current.close()
   }, []);
--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,2 +1,4 @@
 export { default as ChatConnect } from './ChatConnect';
+export { default as ConnectedAlert } from './ConnectedAlert';
+
 export { default as useMessagsList } from './useMessageList';
