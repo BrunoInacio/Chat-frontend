ab20e04caeec793ec5a30ff8db9595f438b8d381
Bruno <brunob.inacio@gmail.com>
2020-11-15 12:28:06 -0300

    Updates

---
+++ precache-manifest.447381d58b19682e9f1c1fa18fc2fbba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b2d77a657e933e99b74f6e9ca312a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ffb83928c955e048093",
+    "url": "/static/js/2.0611d663.chunk.js"
+  },
+  {
+    "revision": "884d6422099fcad6c31f436a36163b39",
+    "url": "/static/js/2.0611d663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ff19e3e85fae11981a",
+    "url": "/static/js/main.6a53a2f5.chunk.js"
+  },
+  {
+    "revision": "06358890af58db31d13f",
+    "url": "/static/js/runtime-main.9ce0fea0.js"
+  }
+]);
