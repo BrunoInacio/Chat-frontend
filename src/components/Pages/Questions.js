9e458dd576f54c4fe92543d578ad4774d6ccc3fe
Bruno <brunob.inacio@gmail.com>
2020-11-22 23:57:45 -0300

    Update questions page

--- src/components/Pages/Questions.js
+++ src/components/Pages/Questions.js
@@ -6,7 +6,7 @@ import withPages from './withPages';
 import FAQ from './Questions.list';
 
 import {
-  Typography, Button,
+  Typography, Button, Box,
   ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, ExpansionPanelActions
 } from '@material-ui/core';
 
@@ -15,30 +15,34 @@ import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
 function Questions({ setTitle }) {
   React.useEffect(() => { setTitle("Dúvidas comuns") }, [setTitle])
 
-  return FAQ.map((item, _) => (
-    <ExpansionPanel key={item.question}>
+  return FAQ.map((topic, _) => (
+      <Box marginBottom={2} marginTop={2}>
+      {topic.questions.map((item, _) => (
+        <ExpansionPanel key={item.question}>
 
-      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography variant="subtitle2">{item.question}</Typography>
-      </ExpansionPanelSummary>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="subtitle2">{item.question}</Typography>
+          </ExpansionPanelSummary>
 
-      <ExpansionPanelDetails>
-        <Typography color="textSecondary">{item.answer}</Typography>
-      </ExpansionPanelDetails>
+          <ExpansionPanelDetails>
+            <Typography color="textSecondary">{item.answer}</Typography>
+          </ExpansionPanelDetails>
 
-      {item.actions ? (
-        <ExpansionPanelActions>
-          {item.actions.map((act, _) => (
-            <Button key={act.text} size="small" color="primary" component={Link} to={act.to}>
-              {act.text}
-            </Button>
-          ))}
-        </ExpansionPanelActions>
-        ) : null
-      }
+          {item.actions ? (
+            <ExpansionPanelActions>
+              {item.actions.map((act, _) => (
+                <Button key={act.text} size="small" color="primary" component={Link} to={act.to}>
+                  {act.text}
+                </Button>
+              ))}
+            </ExpansionPanelActions>
+            ) : null
+          }
 
-    </ExpansionPanel>
-  ))
+        </ExpansionPanel>
+      ))}
+      </Box>
+    ))
 }
 
 export default withPages(Questions);
--- src/components/Pages/Questions.list.js
+++ src/components/Pages/Questions.list.js
@@ -1,72 +1,100 @@
 const questions = [
   {
-    'question': "Como posso testar o Chatbot?",
-    'answer': "Para testar o chatbot basta enviar uma pergunta ou alguma mensagem\
-               de início de conversa e o programa automaticamente responderá\
-               adequadamente.",
-    'actions': [
+    'title': "Questões sobre o projeto",
+    'questions': [
       {
-      'text': "Ir para Chatbot",
-      'to': '/chatbot'
+        'question': "Como posso testar o Chatbot?",
+        'answer': "Para testar o chatbot basta abrir a página de conversa e \
+                   você deverá receber uma mensagem do chatbot indicando algumas \
+                   das ações disponíveis. Fique à vontade para fazer uma pergunta \
+                   ou enviar alguma solicitação.",
+        'actions': [
+          {
+          'text': "Ir para Chatbot",
+          'to': '/chatbot'
+          },
+        ]
+      },
+      {
+        'question': "Como posso entrar em contato com os autores do projeto?",
+        'answer': "Na página referente à Equipe, é possível clicar em cima da foto \
+                   de um dos autores para expandir informações mais detalhadas \
+                   sobre o autor, incluindo informações de contato.",
+        'actions': [
+          {
+          'text': "Ver Equipe",
+          'to': '/team'
+          },
+        ]
+      },
+      {
+        'question': "Quais tecnologias foram utilizadas no projeto?",
+        'answer': "Na página referente às tecnologias é possível observar uma \
+                   lista de tecnologias que foram utilizadas no desenvolvimento \
+                   do projeto. Entre elas estão a linguagem JavaScript, que foi \
+                   utilizada na construção do front-end em conjunto com o React e \
+                   o Material UI. \
+                   Já os chatbots foram construídos em Python com base na biblioteca \
+                   Rasa NLU. Por fim, a \
+                   comunicação entre os serviços foi feita através da biblioteca \
+                   Socket.IO. ",
+        'actions': [
+          {
+          'text': "Ver Tecnologias utilizadas",
+          'to': '/tech'
+          },
+        ]
       },
     ]
   },
   {
-    'question': "O que é um Chatbot?",
-    'answer': "Chatbot é um software de computador que simula conversas \
-               humanas, em forma de texto ou de voz, possibilitando comunicação \
-               entre pessoas e computadores com base em linguagem natural, \
-               fazendo com que o computador se comporte como um ser humano.\
-               \
-               Eles funcionam com base em técnicas de Inteligência Artificial \
-               como Processamento de Linguagem Natural e Aprendizado de Máquina \
-               e podem ser classificados em: (i) declarativos, que são baseados \
-               em regras e focam na execução de apenas uma tarefa; \
-               e (ii) conversacionais, que são mais sofisticados, utilizam mais \
-               técnicas de aprendizado de máquina e podem funcionar como \
-               assistentes virtuais que fornecme sugestões aos usuários e \
-               podem ser capazes de prever demandas",
-  },
-  {
-    'question': "Quais as vantagens do uso de um Chatbot?",
-    'answer': "Uma implementação adequada de atendimento automatizado gera \
-               benefícios para ambas as partes envolvidas. Para a empresa, \
-               é possível atrair público mais jovem, reduzir custos com \
-               centrais de atendimento e realocar funcionários e investimentos \
-               em outras áreas a fim de melhorar a experiência dos usuários e \
-               de gerar maiores retornos. Enquanto para o cliente, é possível \
-               reduzir o tempo gasto com ferramentas de busca e simplificar o \
-               acesso a informações e recomendações.",
-  },
-  {
-    'question': "O que é Processamento de Linguagem Natural (PLN)?",
-    'answer': "O Processamento de Linguagem Natural (PLN) é uma área que da \
-               Inteligência Artificial que interage com a Ciência da Computação \
-               e com a área de linguística visando estudar problemas \
-               relacionados à produção e interpretação de conteúdo em \
-               linguagens humanas (naturais). \
-               \
-               Segundo Russell e Norvig, as linguagens naturais se diferenciam \
-               das linguagens formais por, por exemplo, não conterem um \
-               conjunto definido de cadeias, formadas a partir de uma gramática \
-               com regras definidas, incluírem ambiguidade e mudarem \
-               constantemente. Por estes motivos, a modelagem de linguagens \
-               naturais é mais consistente se feita com o uso de modelos \
-               estatísticos no lugar de regras de produção, como uma gramática \
-               formal.",
-  },
-  {
-    'question': "Como posso entrar em contato com os autores do projeto?",
-    'answer': "Na página referente à Equipe, é possível clicar em cima da foto \
-               de um dos autores para expandir informações mais detalhadas \
-               sobre o autor, incluindo informações de contato.",
-    'actions': [
+    'title': "Questões sobre o projeto",
+    'questions': [
       {
-      'text': "Ver Equipe",
-      'to': '/team'
+        'question': "O que é um Chatbot?",
+        'answer': "Um chatbot é um software capaz de simular conversas humanas,\
+                   possibilitando comunicação entre pessoas e computadores com \
+                   base em linguagem natural, possibilitnado que o computador \
+                   reproduza o comportamento um ser humano. \
+                   Eles funcionam com base em técnicas de Inteligência Artificial,\
+                   como Processamento de Linguagem Natural e Aprendizado de Máquina \
+                   e podem ser classificados em: declarativos, que são baseados \
+                   em regras e focam na execução de apenas uma tarefa, \
+                   e conversacionais, que são mais sofisticados, utilizam mais \
+                   técnicas de aprendizado de máquina e podem funcionar como \
+                   assistentes virtuais que fornecme sugestões aos usuários e \
+                   podem ser capazes de prever demandas.",
+      },
+      {
+        'question': "Quais as vantagens do uso de um Chatbot?",
+        'answer': "Uma implementação adequada de atendimento automatizado gera \
+                   benefícios para ambas as partes envolvidas. Para a empresa, \
+                   é possível atrair público mais jovem, reduzir custos com \
+                   centrais de atendimento e realocar funcionários e investimentos \
+                   em outras áreas a fim de melhorar a experiência dos usuários e \
+                   de gerar maiores retornos. Enquanto para o cliente, é possível \
+                   reduzir o tempo gasto com ferramentas de busca e simplificar o \
+                   acesso a informações e recomendações.",
+      },
+      {
+        'question': "O que é Processamento de Linguagem Natural (PLN)?",
+        'answer': "O Processamento de Linguagem Natural (PLN) é uma área que da \
+                   Inteligência Artificial que interage com a Ciência da Computação \
+                   e com a área de linguística visando estudar problemas \
+                   relacionados à produção e interpretação de conteúdo em \
+                   linguagens humanas (naturais). \
+                   \
+                   Segundo Russell e Norvig, as linguagens naturais se diferenciam \
+                   das linguagens formais por, por exemplo, não conterem um \
+                   conjunto definido de cadeias, formadas a partir de uma gramática \
+                   com regras definidas, incluírem ambiguidade e mudarem \
+                   constantemente. Por estes motivos, a modelagem de linguagens \
+                   naturais é mais consistente se feita com o uso de modelos \
+                   estatísticos no lugar de regras de produção, como uma gramática \
+                   formal.",
       },
     ]
-  },
+  }
 ]
 
 export default questions;
